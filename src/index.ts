ecb866673839b231bd3d80301bd67b359bd3fadd
= <thanhnt@antsomi.com>
2021-10-23 17:07:23 +0700

    Init project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { User } from "./models/User";
+
+const user = new User({ id: 1 });
+user.fetch();
+
+console.log({ user });
---
+++ src/models/User.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosResponse } from "axios";
+
+interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((res: AxiosResponse): void => {
+        this.set(res.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get("id");
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data);
+    }
+  }
+}
